632f8126586326b1910e0cd463be297647ac4deb
fjebaker <fergusbkr@gmail.com>
2024-11-19 22:00:03 +0000

    feat: add dateparser support
    Now use the dateparser library on the `day` command to allow
    colloquialisms to become datetime objects.
    
    Removed the `today` command.

--- thallo/main.py
+++ thallo/main.py
@@ -3,6 +3,7 @@ import json
 from datetime import datetime, timedelta
 
 import click
+import dateparser
 
 from thallo.calendar import Calendar
 
@@ -11,6 +12,9 @@ calendar = Calendar()
 
 def _TODAY():
     return datetime.today().replace(hour=0, minute=0, second=0, microsecond=0)
+
+def _parse_date(s: str) -> datetime:
+    return dateparser.parse(s)
 
 
 @click.group()
@@ -40,6 +44,8 @@ def entry():
     help="Output the fetched events as a JSON string.",
 )
 def fetch(**kwargs):
+    """Fetch events from the calendar and print in various ways.
+    """
     start = kwargs["from"]
     end = kwargs["to"]
 
@@ -61,19 +67,13 @@ def fetch(**kwargs):
 )
 def day(date, **kwargs):
     """Show the events on a specific day. Defaults to today."""
-    events = calendar.fetch(date, date + timedelta(days=1))
+    start = date if date is click.DateTime else _parse_date(date)
+    events = calendar.fetch_dict(start, start + timedelta(days=1))
 
     if kwargs["json"]:
         return print(json.dumps(events))
 
     print(events)
-
-
-@click.command()
-@click.pass_context
-def today(ctx):
-    """Show the events today."""
-    ctx.invoke(day, date=_TODAY())
 
 
 def main():
@@ -82,4 +82,3 @@ def main():
 
 entry.add_command(fetch)
 entry.add_command(day)
-entry.add_command(today)
