2ddda5ba23e82c8598d6eeaa56c88f6f1daf279f
fjebaker <fergusbkr@gmail.com>
2024-11-19 21:14:20 +0000

    feat: format as python executable

---
+++ thallo/main.py
@@ -0,0 +1,13 @@
+from datetime import datetime, timedelta
+
+from thallo.calendar import Calendar
+
+def main():
+
+    today = datetime.today()
+    week_before = today - timedelta(days=7 * 2)
+    week_after = today + timedelta(days=7 * 2)
+
+    cal = Calendar()
+    events = cal.fetch_dict(week_before, week_after)
+    print(events)
