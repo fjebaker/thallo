88ab63c1bd680884c66742032c4c1aa3fe588a77
fjebaker <fergusbkr@gmail.com>
2024-11-21 00:42:02 +0000

    feat(fmt): add margin to the pretty print

--- thallo/format.py
+++ thallo/format.py
@@ -11,10 +11,14 @@ BOX_TOP_LEFT = "╭"
 BOX_CONT = "│"
 BOX_BOT_LEFT = "╰"
 
+INDENT = "  "
+
 TIME_FMT = Fore.YELLOW + Style.BRIGHT
 TIME_END = Fore.RESET + Style.RESET_ALL
 
 def pretty_print_events(events: list[Event]):
+    # new line at the top
+    print()
     for event in events:
         start = datetime.fromisoformat(event["start_time"])
         start_date = start.strftime('%a %d %b %Y')
@@ -25,10 +29,10 @@ def pretty_print_events(events: list[Event]):
         end_date = end.strftime('%a %d %b %Y')
         end_time = end.strftime('%H:%M')
 
-        print(Style.DIM + BOX_TOP_LEFT, start_date + Style.RESET_ALL, TIME_FMT + start_time + TIME_END, end = "")
+        print(INDENT + Style.DIM + BOX_TOP_LEFT, start_date + Style.RESET_ALL, TIME_FMT + start_time + TIME_END, end = "")
         print(" - ", end = "")
         print(Style.DIM + end_date + Style.RESET_ALL,  TIME_FMT + end_time + TIME_END)
-        print(Style.DIM + BOX_BOT_LEFT + Style.RESET_ALL,  Style.BRIGHT + event["name"] + Style.RESET_ALL, end="")
+        print(INDENT + Style.DIM + BOX_BOT_LEFT + Style.RESET_ALL,  Style.BRIGHT + event["name"] + Style.RESET_ALL, end="")
         n = len(event["attendees"])
         print(f" - with {n} attendees")
         print()
